4ce95e65fe096807c0487b7bbdf32768adc06b02
yishai <yishai5555@gmail.com>
2018-10-30 15:53:50 +0200

    first commit - should pass

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World - different message";
+        }
+    }
+}
